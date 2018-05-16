4177df08afd0701fcab3e85d2989ae2545384d5a
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2018-05-16 09:12:15 +0200

    first commit with registration screen

---
+++ app/src/main/java/com/codingstoic/employeevacationapp/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.codingstoic.employeevacationapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
